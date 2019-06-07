952b4c96a10261832bd9ffcbccfbf36c2ff34147
Mark B <mrbruce9866@gmail.com>
2019-06-06 21:36:31 -0400

    calls to census working and pulling correct state

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,16 +6,21 @@ $(document).ready(function (){
         event.preventDefault();
 
         var city = $("#cityInput").val().trim();
-        var state = $("#stateInput").prop("selectedIndex");
-        console.log(state)
-
-        console.log(city,state)
-
+        var state = $("#stateInput").attr("data-select-state");
         getLocationInfo(city, state);
+    });
+
+    $('#stateInput').on('change', function(event) {
+        var $select = $(this);
+        var selectedOption = $select.children()[$select.prop('selectedIndex')];
+        var stateIndexAttr = $(selectedOption).attr('data-index');
+        $select.attr("data-select-state", stateIndexAttr);
     });
 });
 
+
 function getLocationInfo(city, state){
+
     var queryUrl = "https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE=7&for=state:"+state;
 
 
@@ -27,10 +32,18 @@ function getLocationInfo(city, state){
         url: queryUrl,
         method: 'GET'
     }).then(function (response) {
-        console.log(response)
+        
+        var stateName = response[1][0];
+        var population = response[1][1];
 
+        $("#cityInfo").empty();
 
+        var stateHTML = $("<p>").text("State: " + stateName);
+        var popHTML = $("<p>").text("Population: "+ population);
 
+        $("#cityInfo").append(stateHTML, popHTML);
     });
+
+    
 
 }
