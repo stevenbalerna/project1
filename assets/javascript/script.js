da8354f6017a00be496bece454b29f67f9a1f6f7
Mark B <mrbruce9866@gmail.com>
2019-06-11 06:09:35 -0400

    pulled cityInfo from Teleport api and creates HTMl elements to display

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,49 +1,116 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
 
-    $("#submitLocationBtn").on("click", function(event){
+    $("#submitLocationBtn").on("click", function (event) {
 
         event.preventDefault();
 
         var city = $("#cityInput").val().trim();
         var state = $("#stateInput").attr("data-select-state");
+
+        
+
+
+        console.log(city)
+        console.log(state)
         getLocationInfo(city, state);
     });
 
-    $('#stateInput').on('change', function(event) {
+    $('#stateInput').on('change', function (event) {
         var $select = $(this);
         var selectedOption = $select.children()[$select.prop('selectedIndex')];
-        var stateIndexAttr = $(selectedOption).attr('data-index');
-        $select.attr("data-select-state", stateIndexAttr);
+        var stateName = $(selectedOption).text() ? $(selectedOption).text().split(" (")[0] : undefined;
+        $select.attr("data-select-state", stateName);
     });
+
+
 });
 
-
-function getLocationInfo(city, state){
-
-    var queryUrl = "https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&DATE=7&for=state:"+state;
-
-
-    console.log(state)
-    console.log(queryUrl)
-
+function getLocationInfo(city, state) {
+    var queryUrl = "https://api.teleport.org/api/cities/?search=" + city;
 
     $.ajax({
         url: queryUrl,
         method: 'GET'
     }).then(function (response) {
-        
-        var stateName = response[1][0];
-        var population = response[1][1];
+        var link;
+        var results = response._embedded["city:search-results"]
 
-        $("#cityInfo").empty();
+        results.forEach(result => {
+            var name = result.matching_full_name;
+            if (isGoodResult(name, city, state)) {
+                link = result._links["city:item"].href;
+                return;
+            }
+        });
 
-        var stateHTML = $("<p>").text("State: " + stateName);
-        var popHTML = $("<p>").text("Population: "+ population);
-
-        $("#cityInfo").append(stateHTML, popHTML);
+        getCityData(link);
     });
 
-    
+}
 
+function formatLocation(location) {
+    location = location.toLowerCase();
+    location = location.replace(".", "");
+    return location;
+}
+
+function isGoodResult(searchResultName, city, state) {
+    var searchResult;
+    searchResultName = formatLocation(searchResultName);
+    searchResult = searchResultName.split(",");
+    return searchResult[0].trim() === formatLocation(city) && searchResult[1].trim() === state.toLowerCase() && searchResult[2].includes("united states");
+}
+
+function getCityData(link){
+   
+    var cityInfo = {
+        cityName: "",
+        cityPopulation: 0,
+        scoreNames: [],
+        scores: []
+    }
+
+    $.ajax({
+        url: link,
+        method: 'GET'
+    }).then(function (response) {
+        cityInfo.cityName = response.full_name;
+        cityInfo.cityPopulation = response.population;
+
+        if(response._links.hasOwnProperty("city:urban_area")){
+            $.ajax({
+                url: response._links["city:urban_area"].href+"scores",
+                method: 'GET'
+            }).then(function (response) {
+               
+                response.categories.forEach(scoreElement => {
+                    cityInfo.scoreNames.push(scoreElement.name);
+                    var score = Math.round(parseFloat((scoreElement["score_out_of_10"]/2).toFixed(1)) / .5) * .5; 
+                    cityInfo.scores.push(score);
+                });
+                console.log(cityInfo)
+                displayCityInfo(cityInfo);
+                
+            });
+            
+        }else{
+            console.log(cityInfo)
+            displayCityInfo(cityInfo)
+        }
+
+    });
+}
+
+function displayCityInfo(cityInfo){
+    $("#cityInfo").empty();
+    var $container = $("<div>")
+    var $cityName = $("<h3>").text(cityInfo.cityName).appendTo($container);
+    var $cityPopulation = $("<h5>").text("Population: " + cityInfo.cityPopulation).appendTo($container);
+
+    cityInfo.scoreNames.forEach(name => {
+        $container.append($("<h5>").text(name + ": " + cityInfo.scores[cityInfo.scoreNames.indexOf(name)]))
+    });
+
+    $("#cityInfo").append($container);
 }
